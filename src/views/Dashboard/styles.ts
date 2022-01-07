373ec63e57ab3583a1beb7948b75cc336d475b64
davicruzof <daviondejesus@gmail.com>
2022-01-06 22:53:57 -0300

    feat(homescreen): Criando listagem de cards de transações
    Adiconando listagem de cards de transações usando flatList com dados estaticos

--- src/views/Dashboard/styles.ts
+++ src/views/Dashboard/styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components/native';
+import { Platform, FlatList } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
-import { Platform } from 'react-native';
+import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+
+import { DataListProps } from '.';
 
 export const Container = styled.View`
     flex: 1;
@@ -72,3 +74,25 @@ export const HighlightCards = styled.ScrollView.attrs({
    position: absolute;
    margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transactions = styled.View`
+    flex: 1;
+    padding: 0 24px;
+
+    margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+    font-size: ${RFValue(18)}px;
+    font-family: ${({theme}) => theme.fonts.regular};
+    margin-bottom: 16px;
+`;
+
+export const TransactionsList = styled(
+    FlatList as new () => FlatList<DataListProps>
+).attrs({
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle: { 
+        paddingBottom: getBottomSpace()
+    },
+})``;
