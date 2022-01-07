badf0304860c1a58b962c51cef09afc127de5d8d
davicruzof <daviondejesus@gmail.com>
2022-01-06 21:45:37 -0300

    feat(theme): Adicionando theme global
    Adicionando fontes e cores em um tema global

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+import theme from  '@styles/theme';
+import { Dashboard } from '@views/Dashboard';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle='dark-content' 
+        backgroundColor="rgba(0,0,0,0)" 
+        translucent 
+      />
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
---
+++ src/utils/styles/theme.ts
@@ -0,0 +1,21 @@
+export default {
+    colors: {
+        primary: '#5636D3',
+        secondary: '#FF872C',
+        secondary_ligth: 'rgba(255,135,44,0.3)',
+        success: '#12A454',
+        success_ligth: 'rgba(18,164,84,0.5)',
+        attention: '#E83F5B',
+        attention_ligth: 'rgba(232,63,91,0.5)',
+        shape: '#FFFFFF',
+        title: '#363F5F',
+        text: '#969CB2',
+        text_dark: '#000000',
+        background: '#F0F2F5'
+    },
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold'
+    }
+}
