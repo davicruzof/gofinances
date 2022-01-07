d14a97497e84d2aa3caa2264f5d9ce8570eca90d
davicruzof <daviondejesus@gmail.com>
2022-01-06 22:53:57 -0300

    feat(homescreen): Criando listagem de cards de transações
    Adiconando listagem de cards de transações usando flatList com dados estaticos

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,12 +1,54 @@
 import React, { ReactElement } from 'react';
 
-import { Container, Header, HighlightCards,UserWrapper, UserInfo, UserAvatar, Username, User, UserGreeting as UserGreeting, Icon } from './styles';
+import { Container, Header, HighlightCards,UserWrapper, UserInfo, UserAvatar, Username, User, UserGreeting as UserGreeting, Icon, Transactions, Title, TransactionsList } from './styles';
 
-import { HighlightCard } from '@components/HighligthCard';
+import { HighlightCard } from '@components/HighlightCard';
+import { TransactionCard } from '@components/TransactionCard';
 
 const avatar = 'https://avatars.githubusercontent.com/u/47984117?v=4';
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 function Dashboard(): ReactElement {
+
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: 'positive',
+      amount:'R$: 12.000,00',
+      date:'12/03/2021',
+      title:'Desenvolvimento de Site',
+      category:{
+        name: 'Vendas',
+        icon: 'dollar-sign'
+      }
+    },
+    {
+      id: '2',
+      type: 'negative',
+      amount:'R$: 38,00',
+      date:'15/03/2021',
+      title:'Hamburger Pizza',
+      category:{
+        name: 'Alimentação',
+        icon: 'coffee'
+      }
+    },
+    {
+      id: '3',
+      type: 'negative',
+      amount:'R$: 1.200,00',
+      date:'27/03/2021',
+      title:'Aluguel do Apartamento',
+      category:{
+        name: 'Casa',
+        icon: 'home'
+      }
+    }
+  ]
+
   return (
     <Container>
       <Header>
@@ -41,6 +83,17 @@ function Dashboard(): ReactElement {
           lastTransaction='última entrada dia 13 de abril' 
         />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem de Transações</Title>
+        
+        <TransactionsList 
+          data={data}
+          keyExtractor={item => item.id }
+          renderItem={({ item }) => <TransactionCard  data={item} />}
+        />
+      </Transactions>
+
     </Container>
   );
 };
