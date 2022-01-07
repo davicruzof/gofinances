23f9d5106ca82b92922078a3719e26c90a4f326a
davicruzof <daviondejesus@gmail.com>
2022-01-06 21:44:48 -0300

    chore(typescript): Adicionando bibliotecas e configurando o typescript
    Configuração dos path alias, adicionando libs para estilização com styled-componts

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+          'module-resolver',
+          {
+              root: ['./src'],
+              extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+              alias: {
+                  '@styles': './src/utils/styles',
+                  '@views': './src/views',
+                  '@assets': './src/assets',
+                  '@routes': './src/routes',
+                  '@services': './src/services',
+                  '@interfaces': './src/interfaces',
+                  '@components': './src/components'
+              }
+          }
+      ]
+  ]
   };
 };
